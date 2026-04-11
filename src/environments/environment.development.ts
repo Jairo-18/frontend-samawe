83f72966bed2e0c3dfac0b2a847283f8f7a27245
Jairo-18 <jhonlegarda1.2@gmail.com>
2026-04-11 00:32:51 -0500

    changeenviroment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  //apiUrl: 'https://apidev.ecohotelsamawe.com/',
-  apiUrl: 'http://localhost:3000/',
+  apiUrl: 'https://apidev.ecohotelsamawe.com/',
+  //apiUrl: 'http://localhost:3000/',
   production: false,
   googleMapsApiKey: 'AIzaSyBUEiyCQTzVdjgR8MVnts1VqtvA7lZvGdk'
 };
